f992a344193942b46a83475d101bf7cd7e3a2058
Nick Thomas <jesuswaffle@gmail.com>
2007-02-20 01:09:18 +0000

    Check in some stray changes from forever ago.

--- src/client.lisp
+++ src/client.lisp
@@ -182,7 +182,15 @@ tail calls. Life would be simpler if we had continuations."
 		(release-lock input-lock)
 		nil))))
 
-;; XXX: Define a macro to define wrappers over WITH-CLIENT-INPUT.
+(defmacro def-input-processor (name input-var function-var lambda-list
+			       &body body)
+  "Defines a new input processor macro."
+  (let ((function-name (intern (format nil "%~A" (symbol-name name))
+			       (symbol-package name)))) 
+  `(progn
+    (defmacro ,name (,var ,lambda-list &body body)
+      `(,',function-name #'(lambda (,var) ,@body)))
+    (defun ,function-name ,(cons function-var lambda-list)
 
 (defun client-message (format &rest args)
   "Sends a message to the client from a client thread."
